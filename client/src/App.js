b7b66f0afca603d80dda95e8f6ea173debc61194
LevonAr <levonrm10@gmail.com>
2021-04-18 19:30:55 -0700

    first route done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+// include   <BrowserRouter>
+
 function App() {
   return (
     <div className="App">
---
+++ client/src/components/SideBar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "antd/dist/antd.css";
+import { Layout, Menu, Breadcrumb } from "antd";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+
+const { Sider } = Layout;
+const { SubMenu } = Menu;
+
+export default class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false,
+    };
+  }
+
+  onCollapse = (collapsed) => {
+    console.log(collapsed);
+    this.setState({ collapsed: !this.state.collapsed });
+  };
+
+  render() {
+    return (
+      <Sider
+        collapsible
+        collapsed={this.state.collapsed}
+        onCollapse={this.onCollapse}
+      >
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            <Link to="/directory">Directory</Link>
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            <Link to="/analysis">Analysis</Link>
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+    );
+  }
+}
